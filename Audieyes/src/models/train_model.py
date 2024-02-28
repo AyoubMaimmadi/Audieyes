fded970960d973391642ccd0e9e1e297b3c7d5bc
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2024-02-28 17:39:49 +0100

    update

--- Audieyes/src/models/train_model.py
+++ Audieyes/src/models/train_model.py
@@ -1,4 +1,5 @@
 import mlflow
+import os
 
 def train_model(learning_rate, batch_size):
     # Start MLflow run
@@ -6,8 +7,6 @@ def train_model(learning_rate, batch_size):
         # Log parameters
         mlflow.log_param("learning_rate", learning_rate)
         mlflow.log_param("batch_size", batch_size)
-        mlflow.log_param("dataset", "COCO_dataset")  # Log dataset name
-        mlflow.log_param("model", "first_model")    # Log model name
 
         # Simulate training and log metrics
         accuracy = 0.85
@@ -15,8 +14,35 @@ def train_model(learning_rate, batch_size):
         mlflow.log_metric("accuracy", accuracy)
         mlflow.log_metric("loss", loss)
 
-        # Log artifacts (e.g., model file)
-        # mlflow.log_artifact("model.pkl")
+        metrics = {
+            "TR@1": 0.80, # Text Retrieval @1
+            "IR@1": 0.63, # Image Retrieval @1
+            "TR@5": 0.95, # Text Retrieval @5
+            "IR@5": 0.85, # Image Retrieval @5
+            "TR@10": 0.97, # Text Retrieval @10
+            "IR@10": 0.91, # Image Retrieval @10
+            "B@4": 38.0, # BLEU Score
+            "CIDEr": 127.8, # CIDEr Score
+            "SPICE": 13.9, # SPICE Score
+        }
+
+        for metric_name, metric_value in metrics.items():
+            mlflow.log_metric(metric_name, metric_value)
+
+        # Log dataset configuration as an artifact
+        dataset_config_path = "C:/Users/Ayoub Maimmadi/Documents/AUI/Asmae Mourhir/Audieyes/Audieyes/data/pretrain.yaml"
+        if os.path.exists(dataset_config_path):
+            mlflow.log_artifact(dataset_config_path, "dataset_config")
+        else:
+            print("Dataset configuration file not found at specified path:", dataset_config_path)
+        
+        # Log model file as an artifact
+        model_path = "C:/Users/Ayoub Maimmadi/Documents/AUI/Asmae Mourhir/Audieyes/Audieyes/models/model_base.pth"
+        if os.path.exists(model_path):
+            mlflow.log_artifact(model_path, "model_checkpoint")
+        else:
+            print("Model file not found at specified path:", model_path)
+
 
 if __name__ == "__main__":
     # Define hyperparameters
