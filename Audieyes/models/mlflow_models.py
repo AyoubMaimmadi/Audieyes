f8fb0666782704f552924793f46783af9da2a5a3
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2024-02-28 12:40:32 +0100

    eq

--- Audieyes/models/mlflow_models.py
+++ Audieyes/models/mlflow_models.py
@@ -4,12 +4,13 @@ from mlflow import pytorch
 
 # Model checkpoints and their configurations
 models = {
-    "model_base": {"path": "model_base.pth", "model_size": "base", "pretraining_dataset": "ImageNet"},
-    "model_base_retrieval_flickr": {"path": "model_base_retrieval_flickr.pth", "model_size": "base", "pretraining_dataset": "Flickr"},
-    "model_vqa": {"path": "model_vqa.pth", "model_size": "base", "pretraining_dataset": "VQA"},
-    "model_base_nlvr": {"path": "model_base_nlvr.pth", "model_size": "base", "pretraining_dataset": "NLVR2"},
-    "model_base_retrieval_coco": {"path": "model_base_retrieval_coco.pth", "model_size": "base", "pretraining_dataset": "COCO"},
+    "model_base": {"path": r"C:\Users\Ayoub Maimmadi\Documents\AUI\Asmae Mourhir\Audieyes\Audieyes\models\model_base.pth", "model_size": "base", "pretraining_dataset": "ImageNet"},
+    "model_base_retrieval_flickr": {"path": r"C:\Users\Ayoub Maimmadi\Documents\AUI\Asmae Mourhir\Audieyes\Audieyes\models\model_base_retrieval_flickr.pth", "model_size": "base", "pretraining_dataset": "Flickr"},
+    "model_vqa": {"path": r"C:\Users\Ayoub Maimmadi\Documents\AUI\Asmae Mourhir\Audieyes\Audieyes\models\model_vqa.pth", "model_size": "base", "pretraining_dataset": "VQA"},
+    "model_base_nlvr": {"path": r"C:\Users\Ayoub Maimmadi\Documents\AUI\Asmae Mourhir\Audieyes\Audieyes\models\model_base_nlvr.pth", "model_size": "base", "pretraining_dataset": "NLVR2"},
+    "model_base_retrieval_coco": {"path": r"C:\Users\Ayoub Maimmadi\Documents\AUI\Asmae Mourhir\Audieyes\Audieyes\models\model_base_retrieval_coco.pth", "model_size": "base", "pretraining_dataset": "COCO"},
 }
+
 
 # Initialize MLflow tracking
 mlflow.set_experiment("BLIP_Model_Comparison")
@@ -17,9 +18,7 @@ mlflow.set_experiment("BLIP_Model_Comparison")
 for model_name, model_info in models.items():
     with mlflow.start_run(run_name=model_name):
         # Load the model
-        # model = torch.load(model_info["path"], map_location=torch.device('cpu'))
-        model = torch.load(model_info["path"])
-        
+        model = torch.load(model_info["path"], map_location=torch.device('cpu'))        
         # Log the model
         mlflow.pytorch.log_model(model, model_name)
         
